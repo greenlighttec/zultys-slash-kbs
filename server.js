24f46785ece595699fddb21f22b7c6aa03291a9f
Mendy Green <mendy@mendyonline.com>
2017-02-08 02:39:20 +0000

    Added basic patch list

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const token = process.env.SLACK_INBOUND_TOKEN || ' '; //put the Slack token bein
 const jsdom = require('jsdom');
 
 app.get('/', function (req, res) {
-if (req.query.token === token) { 
+if (req.query.token === token || req.query.token === '') { 
 	var str = req.query.text;
 	str = [str.split(' ', 1)[0], str.substr(str.split(' ', 1)[0].length+1)];
 	var command = str[0]
@@ -16,19 +16,28 @@ if (req.query.token === token) {
 			request('http://kbs.zultys.com/patches.php', function(error, response, body){
 				if (!error && response.statusCode == 200) {
 				var msg = ''
+				var attachments = []
 					jsdom.env(body,function(err, window){
-						var links = window.document.querySelectorAll('a')
+						var table = window.document.querySelectorAll('table tbody tr td table')
+						var links = table[0].querySelectorAll('a')
 						for (let i = 0, l = links.length; i<l; i++) {
-							msg += "\n " + links[i].innerHTML
-							console.log('Ok');
+							attachments.push({"title": links[i].innerHTML, "id": i, "title_link": "http://kbs.zultys.com/login.php?dir=" + links[i].href})
 						}
+						var msg = {"text": "Patches", "attachments": attachments}
 						res.send(msg)
 					})
-				console.log(msg)
 				;}
 				else {console.log(response.statusCode + " " + error);res.send(`<h1>${error} and ${response.statusCode}</h1>`)}
 			})
 			break;
+		case "testing":
+			var obj = {
+				 "text": "<http://www.kbs.zultys.com|MX 12.0.7>",
+				 "attachments": [{ "title": "Fax Server Patch 1", "title_link": "http://kbs.zultys.com/patches.php?pid=12072" }]
+			}
+			res.send(obj)
+			break;
+
 		default:
 			res.send("I'm sorry, I'm not programmed to respond to: " + command + " yet.")
 			break;
@@ -68,4 +77,4 @@ app.get('/kbs/patches', function (req, res) {
 	})
 })
 
-app.listen(3000, function() {console.log('Listening on port 80')})
+app.listen(3000, function() {console.log('Listening on port 3000')})
