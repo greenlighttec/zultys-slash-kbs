1aeecde50751e6dbff717ca0a3ea612c3e408321
Mendy Green <mendy@mendyonline.com>
2017-02-10 17:51:00 -0500

    KBS Search v1

--- server.js
+++ server.js
@@ -13,14 +13,7 @@ app.get('/', function (req, res) {
 	var query = str[1]
 	switch (true) {
 		case (command.test("patches")):
-			request('http://kbs.zultys.com/patches.php', callback); 
-			break;
-		case (command.test("testing")):
-			var obj = {
-				 "text": "<http://www.kbs.zultys.com|MX 12.0.7>",
-				 "attachments": [{ "title": "Fax Server Patch 1", "title_link": "http://kbs.zultys.com/login.php?dir=patches.php?pid=12072" }]
-			}
-			res.send(obj)
+			request('http://kbs.zultys.com/patches.php', callbackPatches); 
 			break;
 		case (command.test("search")):
 			var options = {
@@ -29,15 +22,55 @@ app.get('/', function (req, res) {
 			'		Authorization': "Basic " + auth
 				}
 			};
-			request(options, callback);
+			request(options, callbackSearch);
 			break;
-
+		case (command.test("testing")):
+			var obj = {
+				 "text": "<http://www.kbs.zultys.com|MX 12.0.7>",
+				 "attachments": [{ "title": "Fax Server Patch 1", "title_link": "http://kbs.zultys.com/login.php?dir=patches.php?pid=12072" }]
+			}
+			res.send(obj)
+			break;
 		default:
 			res.send("I'm sorry, I'm not programmed to respond to: " + command + " yet.")
 			break;
 	}
-	
-			function callback(error, response, body){
+			function callbackSearch(error, response, body){
+				if (!error && response.statusCode == 200) {
+				var msg = ''
+				var results = []
+					jsdom.env(body,function(err, window){
+						var table = window.document.getElementsByClassName('issues-text')
+						var arr = [].slice.call(table);
+						var results = arr.filter((item) => {return item.style.background !== ""});
+						var links = []
+						for (let i = 0, l = results.length; i<l; i++) {
+							links.push(results[i].getElementsByTagName('a'))
+							results.push({"author_name": results[i].getElementsByTagName('td')[0].innerHTML,
+							"title": links[i][0].innerHTML,
+							"id": i, 
+							"title_link": "http://kbs.zultys.com/login.php?dir=" + links[i][0].href})
+						}
+						if (query) {
+							var search =  new RegExp('query');
+							var searchResults = []
+							for (let i = 0,l = results.length; i<l;i++){
+								if (search.test(results[i].author_name)) {
+									searchResults.push(results[i])
+									}}
+										if (searchResults.length < 1) {
+											searchResults.push({"title": "No article match with that filter, please try again"})
+										}
+								msg = {"text": "Article " + query, "attachments": searchResults}
+									} 
+							else { msg = {"text": "Articles", "attachments": results} }
+						res.send(msg)
+					})
+				}
+				else {console.log(response.statusCode + " " + error);res.send(`<h1>${error} and ${response.statusCode}</h1>`)}
+			}
+
+			function callbackPatches(error, response, body){
 				if (!error && response.statusCode == 200) {
 				var msg = ''
 				var attachments = []
@@ -76,23 +109,51 @@ app.get('/', function (req, res) {
 
 
 app.get('/kbs/search', function (req, res) {
-	var search = 'sip'
+	var query = 'sip'
 	var options = {
-		url: `http://kbs.zultys.com/issues.php?sstr=${search}&search=>&snm=Search+Results&sid=src`,
+		url: `http://kbs.zultys.com/issues.php?sstr=${query}&search=>&snm=Search+Results&sid=src`,
 		headers: {
 			'Authorization': "Basic " + auth
 		}
 	};
 
-	function callback(error, response, body) {
-		if (!error && response.statusCode == 200) {
-			res.send(body)
-			console.log("Ok " + body)
-		}
-		else {console.log(response);res.send(`<h1>${error} and ${response.statusCode}</h1>`)}
-	}
+			function callbackSearch(error, response, body){
+				if (!error && response.statusCode == 200) {
+				var msg = ''
+				var results = []
+					jsdom.env(body,function(err, window){
+						var table = window.document.getElementsByClassName('issues-text')
+						var arr = [].slice.call(table);
+						var patches = arr.filter((item) => {return item.style.background !== ""});
+						var links = []
+						for (let i = 0, l = patches.length; i<l; i++) {
+							links.push(patches[i].getElementsByTagName('a'))
+							results.push({"author_name": patches[i].getElementsByTagName('td')[0].innerHTML,
+							"title": links[i][0].innerHTML,
+							"id": i, 
+							"title_link": "http://kbs.zultys.com/login.php?dir=" + links[i][0].href})
+						}
+						if (query) {
+							var search =  new RegExp('query');
+							var searchResults = []
+							for (let i = 0,l = results.length; i<l;i++){
+								if (search.test(results[i].author_name)) {
+									searchResults.push(results[i])
+									}}
+										if (searchResults.length < 1) {
+											searchResults.push({"title": "No article match with that filter, please try again"})
+										}
+								msg = {"text": "Article " + query, "attachments": searchResults}
+									} 
+							else { msg = {"text": "Articles", "attachments": results} }
+						res.send(msg)
+					})
+				}
+				else {console.log(response.statusCode + " " + error);res.send(`<h1>${error} and ${response.statusCode}</h1>`)}
+			}
+	//}
 
-	request(options, callback);
+	request(options, callbackSearch);
 
 })
 
